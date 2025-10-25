c635b09c5f2c21433079d88985a986802001e14a
viroloxx <147020861+viroloxx@users.noreply.github.com>
2025-10-25 18:53:46 -0400

    funcion Carga en Json
    Added JavaDoc comment for the guardarResultados method.

--- src/main/java/persistencia/PersistenciaJSON.java
+++ src/main/java/persistencia/PersistenciaJSON.java
@@ -1,6 +1,6 @@
 package persistencia;
 
-import estructura_datos.Lista;
+import estructura_datos.Lista; 
 import modelo.PCB;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,21 +11,28 @@ import java.io.IOException;
  *
  * @author Diego A. Vivolo
  */
-
 public class PersistenciaJSON {
 
-    
+    /**
+     * Guarda la lista de procesos terminados en un archivo JSON.
+     * @param terminados La lista (de tu estructura_datos) de PCBs terminados.
+     * @param ruta La ruta completa del archivo donde se guardará.
+     */
     public static void guardarResultados(Lista terminados, String ruta) throws IOException {
         
-        PCB[] procesosArray = terminados.toArray();
+
+        PCB[] procesosArray = terminados.toArray(); 
+        
         
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         
-
+        
         try (FileWriter writer = new FileWriter(ruta)) {
             
-
+        
             gson.toJson(procesosArray, writer);
         }
+        
     }
+
 }
